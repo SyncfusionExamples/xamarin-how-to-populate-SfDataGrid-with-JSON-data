b7c24e94058f99bc9a0ed9f40c6e837a0530c47c
Sivaraman <sivaraman.sivagurunathan@syncfusion.com>
2020-04-06 17:29:06 +0530

    i have json sample

---
+++ GettingStarted/MainActivity.cs
@@ -0,0 +1,70 @@
+﻿
+using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Views;
+using Android.Content;
+using Android.Graphics;
+using System.Collections;
+using static Android.Views.ViewGroup;
+using Syncfusion.SfDataGrid;
+using System;
+
+namespace GettingStarted
+{
+    [Activity(Label = "WithUnBoundCode", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        SfDataGrid grid;
+        ViewModel viewModel;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            grid = new SfDataGrid(this);
+            viewModel = new ViewModel();
+            grid.AllowSorting = true;
+            grid.AutoGenerateColumns = false;
+            grid.ColumnSizer = ColumnSizer.Star;
+            grid.ItemsSource = viewModel.DynamicCollection;
+            grid.Columns.Add(new GridTextColumn()
+            {
+                HeaderText = "Order ID",
+                MappingName = "OrderID",
+                LineBreakMode = LineBreakMode.WordWrap,
+                TextAlignment = Android.Views.GravityFlags.Left,
+                HeaderTextAlignment = Android.Views.GravityFlags.Left,
+            });
+
+            grid.Columns.Add(new GridTextColumn()
+            {
+                HeaderText = "Customer ID",
+                MappingName = "EmployeeID",
+                LineBreakMode = LineBreakMode.WordWrap,
+                TextAlignment = Android.Views.GravityFlags.Left,
+                HeaderTextAlignment = Android.Views.GravityFlags.Left,
+            });
+
+            grid.Columns.Add(new GridTextColumn()
+            {
+                HeaderText = "First Name",
+                MappingName = "FirstName",
+                LineBreakMode = LineBreakMode.WordWrap,
+                TextAlignment = Android.Views.GravityFlags.Left,
+                HeaderTextAlignment = Android.Views.GravityFlags.Left,
+            });
+
+            grid.Columns.Add(new GridTextColumn()
+            {
+                HeaderText = "Last Name",
+                MappingName = "LastName",
+                LineBreakMode = LineBreakMode.WordWrap,
+                TextAlignment = Android.Views.GravityFlags.Left,
+                HeaderTextAlignment = Android.Views.GravityFlags.Left,
+            });
+
+            SetContentView(grid);
+        }
+    }
+}
+
+
---
+++ GettingStarted/ViewModel/ViewModel.cs
@@ -0,0 +1,44 @@
+﻿//using Newtonsoft.Json;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Dynamic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GettingStarted
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+
+        public const string jsonData = "[{\"OrderID\":1,\"EmployeeID\":100,\"FirstName\":'Gina',\"LastName\":'Gable'}," +
+                                      "{\"OrderID\":2,\"EmployeeID\":200,\"FirstName\":'Danielle',\"LastName\":'Rooney'}," +
+                                     "{\"OrderID\":3,\"EmployeeID\":300,\"FirstName\":'Frank',\"LastName\":'Gable'},]";
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableCollection<dynamic> DynamicCollection { get; set; }
+        public List<ExpandoObject> DynamicJsonCollection { get; set; }
+
+        public ViewModel()
+        {
+            DynamicJsonCollection = JsonConvert.DeserializeObject<List<ExpandoObject>>(jsonData);
+            DynamicCollection = PopulateData();
+        }
+
+        private ObservableCollection<dynamic> PopulateData()
+        {
+            var data = new ObservableCollection<dynamic>();
+            foreach (var item in DynamicJsonCollection)
+            {
+                data.Add(item);
+            }
+            return data;
+        }
+    }
+}
